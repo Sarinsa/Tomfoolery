471c00f292ecb5bd5351e1e7bb5c891a24be7449
Sarinsa <mikkemusbruker@gmail.com>
2021-05-12 20:07:13 +0200

    Set up a proper workspace.

---
+++ src/main/java/com/teamaurora/orespawn/common/core/Orespawn.java
@@ -0,0 +1,17 @@
+package com.teamaurora.orespawn.common.core;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod(Orespawn.MODID)
+public class Orespawn {
+
+    public static final String MODID = "orespawn";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Orespawn() {
+
+    }
+}
